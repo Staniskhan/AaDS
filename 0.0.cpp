5399e94329e61b2e4b585d8c36a12a639bcf6d49
Stanislav Khanin <andromedas82000@gmail.com>
2025-09-08 10:34:50 +0300

    initial commit

---
+++ 0.0.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	ifstream in;
+	in.open("input.txt");
+	if (!in.is_open())
+	{
+		cout << "File did not open properly. Try again.";
+		return 1;
+	}
+
+	int current;
+	long long answer = 0;
+	set<int> arr;
+	while (in >> current)
+	{
+		arr.insert(current);
+	}
+	in.close();
+
+	for (int i : arr)
+	{
+		answer += i;
+	}
+
+	cout << answer << "\n";
+	ofstream out;
+	out.open("output.txt");
+	out << answer;
+	out.close();
+}
