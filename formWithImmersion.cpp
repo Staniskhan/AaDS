f0318ecce8404d59cdd048e68c3ed3de80143dd2
Staniskhan <andromedas82000@gmail.com>
2025-11-26 12:43:33 +0300

    initial commit to formWithImmersion.cpp. start developing

---
+++ formWithImmersion.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+
+int check(int** matr, int** indarr, int fromLine, int fromCol, int n, int m)
+{
+	for (int i = 1; i < n - 1; i++)
+	{
+		for (int j = 1; j < n - 1; j++)
+		{
+			if (indarr[i - 1][j - 1] == 0 && matr[i][j] <= 0)
+			{
+				if (matr[i][j + 1] <= 0 && indarr[i - 1][j] == 1)
+				{
+					//change indarr
+					//return -1
+				}
+				else if (matr[i][j + 1] <= 0 && indarr[i - 1][j] == 0)
+				{
+					//cr(right)
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	std::ifstream in("in.txt");
+	int n, m;
+	in >> n >> m;
+
+	int min = 0;;
+	int** matr = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		matr[i] = new int[n];
+
+		for (int j = 0; j < n; j++)
+		{
+			in >> matr[i][j];
+			if (matr[i][j] < min) min = matr[i][j];
+		}
+	}
+
+	in.close();
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			matr[i][j] -= min;
+		}
+	}
+
+
+}
